058c9424f07c8fcf8ff82d2c40f05828907f3d2c
Lexi <144337198+alex07308@users.noreply.github.com>
2023-10-04 17:16:38 +0300

    code_ball.py

---
+++ code_ball.py
@@ -0,0 +1,26 @@
+import math
+
+def degree(a: float, t: float, r: float, V: float) -> float:
+    """Calculating the angular displacement
+
+    Args:
+        a (float): acceleration
+        t (float): time
+        r (float): radius
+        V (float): speed
+
+    Returns:
+        float: the angular displacement
+    """
+    S = V * t + (a * t ** 2) / 2
+    C = 2 * math.pi * r
+    radians = S / C
+    degrees = math.degrees(radians)
+    return degrees
+
+# a = 10
+# t = 100
+# r = 2
+# V = 1000
+# result = degree(a, t, r, V)
+# print(result)
